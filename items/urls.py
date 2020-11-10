7ebdabe20be1f3564ad1e324e2ac10a1d647c309
pletain <pletain@dgu.ac.kr>
2020-11-10 22:20:49 +0900

    first

--- items/urls.py
+++ items/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create/', views.create, name="create"),
     path('show/<int:post_id>/', views.show, name="show"),
     path('delete/<int:post_id>/', views.delete, name="delete"),
+    path('like_toggle/<int:post_id>/', views.like_toggle, name="like_toggle"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import Post,Comment,Like
+from .models import *
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+import json 
 
 def main(request):
     items = Post.objects.all()
@@ -30,3 +33,23 @@ def delete(request,post_id):
     post = get_object_or_404(Post, pk=post_id)
     post.delete()
     return redirect('main')
+
+@require_POST
+@login_required
+def like_toggle(request, post_id):
+
+    post = get_object_or_404(Post, pk=post_id)
+    post_like, post_like_created = Like.objects.get_or_create(user=request.user, post = post)
+
+    if not post_like_created:
+        post_like.delete()
+        result = "like_cancel"
+    else:
+        result = "like"
+
+    context = {
+            "like_count": post.like_count,
+            "result": result
+    }
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
